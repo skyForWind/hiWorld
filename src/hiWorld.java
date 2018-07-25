124e82afcba279df8f95f1912904b2657d90e4d6
skyForWind <1455545667@qq.com>
2018-07-25 21:44:22 +0800

    test

--- src/hiWorld.java
+++ src/hiWorld.java
@@ -2,5 +2,6 @@ public class hiWorld {
     public static void main(String args[]){
         System.out.println("hi World");
         System.out.println("这是我用Git上传的第一个项目");
+        System.out.print("不如意之事十之八九");
     }
 }
